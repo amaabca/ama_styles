b8702728a70855bdb8dcf6444e639d2d412fd0e6
Ruben Estevez <ruben.a.estevez@gmail.com>
2017-02-15 11:20:06 -0700

    Break the config and environment into two files
    * Because we were getting load issues when runnign test the envs on the
    engine were overwriting the dummy app envs.
    * Add missing configuration to the dummy.

--- Rakefile
+++ Rakefile
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require File.expand_path('../config/application', __FILE__)
-require_relative 'lib/ama/styles/internal/base'
 require 'rspec/core/rake_task'
 require 'rubocop/rake_task'
-AMA::Styles::Application.load_tasks
-AMA::Styles::Application.initialize!
 RSpec::Core::RakeTask.new(:spec)
 RuboCop::RakeTask.new
 task default: [:spec, :rubocop]
--- lib/ama/styles/internal/config.rb
+++ lib/ama/styles/internal/config.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-Rails.application.configure do
-  config.eager_load = false
-  config.assets.css_compressor = :sass
-  config.assets.precompile += %w(shared.css)
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.redis_endpoint = ENV['REDIS_ENDPOINT']
-  config.api_deployment_url = ENV['API_DEPLOYMENT_URL']
-  config.api_deployment_user = ENV['API_DEPLOYMENT_USER']
-  config.api_deployment_password = ENV['API_DEPLOYMENT_PASSWORD']
-  config.cloudfront_url = ENV['ASSET_CLOUDFRONT_URL']
-  config.assets_bucket_name = ENV['S3_ASSETS_BUCKET_NAME']
-end
-
 Aws.config.update(
   region: 'us-west-2',
   credentials: Aws::Credentials.new(
