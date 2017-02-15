6bd0ca366dfc423bb8d9f073fd5fd0407c6a3124
Ruben Estevez <ruben.a.estevez@gmail.com>
2017-02-15 08:53:32 -0700

    Change fetch env read

--- lib/ama/styles/internal/config.rb
+++ lib/ama/styles/internal/config.rb
@@ -4,14 +4,14 @@ Rails.application.configure do
   config.eager_load = false
   config.assets.css_compressor = :sass
   config.assets.precompile += %w(shared.css)
-  config.aws_access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID')
-  config.aws_secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  config.redis_endpoint = ENV.fetch('REDIS_ENDPOINT')
-  config.api_deployment_url = ENV.fetch('API_DEPLOYMENT_URL')
-  config.api_deployment_user = ENV.fetch('API_DEPLOYMENT_USER')
-  config.api_deployment_password = ENV.fetch('API_DEPLOYMENT_PASSWORD')
-  config.cloudfront_url = ENV.fetch('ASSET_CLOUDFRONT_URL')
-  config.assets_bucket_name = ENV.fetch('S3_ASSETS_BUCKET_NAME')
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  config.redis_endpoint = ENV['REDIS_ENDPOINT']
+  config.api_deployment_url = ENV['API_DEPLOYMENT_URL']
+  config.api_deployment_user = ENV['API_DEPLOYMENT_USER']
+  config.api_deployment_password = ENV['API_DEPLOYMENT_PASSWORD']
+  config.cloudfront_url = ENV['ASSET_CLOUDFRONT_URL']
+  config.assets_bucket_name = ENV['S3_ASSETS_BUCKET_NAME']
 end
 
 Aws.config.update(
