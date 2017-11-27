8c7ef720ffd147639fb3e3677adffd26ffd4498f
zjullion <zach.jullion@ama.ab.ca>
2017-11-27 15:54:09 -0700

    Replace factory_girl with factory_bot (#69)
    Replace factory_girl with factory_bot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../spec/dummy/config/environment.rb', __FILE__)
 require 'ama/styles/internal/base'
 require 'webmock/rspec'
-require 'factory_girl'
+require 'factory_bot'
 require 'pry'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
@@ -13,12 +13,12 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 WebMock.disable_net_connect!
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.include Helpers::Fixtures
   config.include Helpers::Requests
 
   config.before(:suite) do
-    FactoryGirl.find_definitions
+    FactoryBot.find_definitions
   end
 
   config.full_backtrace = true
