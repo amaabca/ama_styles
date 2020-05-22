aa94aad7c627874fbea8a80c0cc6747ba513d5b0
darko.dosenovic@ama.ab.ca <darko.dosenovic@ama.ab.ca>
2020-05-22 11:59:30 -0600

    Adding github actions

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -4,3 +4,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.eager_load = false
 Rails.application.config.assets.css_compressor = :sass
 Rails.application.config.assets.precompile += %w(shared.css)
+Rails.configuration.stylesheet_resolver.remote = false
